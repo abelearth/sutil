35cdeb9ba57550d5af6f3aa91fefda692c9cb791
Abel Earth <abelearth@gmail.com>
2015-03-18 14:58:44 +0800

    Improve remote sync script.

---
+++ src/pushw.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+syncwindy.sh -i "$@"
--- src/syncwindy.sh
+++ src/syncwindy.sh
@@ -1,5 +1,27 @@
-if [ "$1"=="-i" ]; then
-rsync -avz -e ssh $2 windy@192.168.1.2:/home/windy/$3
+#!/bin/bash
+
+#set -x
+
+REMOTE_MACHINE=windy@192.168.1.2:/home/windy
+
+if [ "X$1" = "X-i" ]; then
+    if [ -f $2 ]; then
+        SOURCE=$2
+    else
+        SOURCE=""
+    fi
+    DEST=$REMOTE_MACHINE/$3
 else
-rsync -avz -e ssh windy@192.168.1.2:/home/windy/$1 $2
+    SOURCE=$REMOTE_MACHINE/$1
+    if [ -d $2 ]; then
+        DEST=$2
+    else
+        DEST=""
+    fi
+fi
+
+if [ -n "$SOURCE" -a -n "$DEST" ]; then
+    rsync -avz -e ssh "$SOURCE" "$DEST"
+else
+    echo "Parameters error!"
 fi
