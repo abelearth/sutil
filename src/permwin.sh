0f8cc8692d8a15dee687aa835920c533ec3244e9
Abel Earth <abelearth@gmail.com>
2018-12-13 12:01:09 +0800

    Enhanced permwin.

--- src/permwin.sh
+++ src/permwin.sh
@@ -1,2 +1,4 @@
-find $1 -iname "*.sh" -o -iname "*.exe" -o -iname "*.bat" | xargs -I{} chmod -v +x {}
+find $1 -type d | xargs -I{} chmod -v 0775 {}
+find $1 -not -type d | xargs -I{} chmod -v 0664 {}
+find $1 -iname "*.sh" -o -iname "*.py" -o -iname "*.exe" -o -iname "*.bat" -o -iname "*.dll" -o -iname "*.a" | xargs -I{} chmod -v +x {}
 
